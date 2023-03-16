ab140283dd070d7595f2e7424610015fb2583354
ktynski <kristin@frac.tl>
2023-03-16 14:17:38 -0400

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import openai
 import os
 import streamlit as st
 
-
 # Custom color scheme
 st.markdown(
     """
@@ -11,9 +10,6 @@ st.markdown(
         background-color: #f0f0f0;
     }
     .main {
-        background-color: #f0f0f0;
-    }
-    .sidebar .sidebar-content {
         background-color: #f0f0f0;
     }
     h2 {
@@ -26,22 +22,23 @@ st.markdown(
 
 st.title("Lesson Plan & Class Materials Generator")
 
+with st.sidebar:
+    st.header("API Key")
+    openai.api_key = st.text_input("Enter your OpenAI API Key", type="password")
+
 grade_levels = [f"Grade {i}" for i in range(1, 13)] + ["Kindergarten"]
 subjects = ["Math", "Science", "English", "History", "Social Studies"]
 durations = ["30 minutes", "45 minutes", "60 minutes", "90 minutes"]
 complexity_levels = ["Beginner", "Intermediate", "Advanced"]
 
 # Input options
-api_key = st.text_input("Enter your OpenAI API Key", type="password")
-grade_level = st.selectbox("Grade Level", options=grade_levels)
-subject = st.selectbox("Subject", options=subjects)
-topic = st.text_input("Topic")
-duration = st.selectbox("Duration", options=durations)
-complexity = st.selectbox("Complexity Level", options=complexity_levels)
+cols = st.beta_columns(4)
+grade_level = cols[0].selectbox("Grade Level", options=grade_levels)
+subject = cols[1].selectbox("Subject", options=subjects)
+duration = cols[2].selectbox("Duration", options=durations)
+complexity = cols[3].selectbox("Complexity Level", options=complexity_levels)
 techniques = st.multiselect("Pedagogical Techniques", options=["Bloom's Taxonomy", "Project-Based Learning", "Differentiated Instruction", "Inquiry-Based Learning"])
 learning_styles = st.multiselect("Learning Styles", options=["Visual", "Auditory", "Kinesthetic"])
-
-openai.api_key = api_key
 
 def generate_lesson_plan_and_materials(grade_level, subject, topic, duration, techniques, complexity, learning_styles):
     techniques_str = ', '.join(techniques)
@@ -64,9 +61,12 @@ if st.button("Generate Lesson Plan & Class Materials"):
     if not topic:
         st.error("Please enter a topic.")
     else:
-        output = generate_lesson_plan_and_materials(grade_level, subject, topic, duration, techniques, complexity, learning_styles)
-        lesson_plan, class_materials = output.split('\n\nClass Materials\n\n')
-        st.header("Generated Lesson Plan")
-        st.write(lesson_plan)
-        st.header("Generated Class Materials")
-        st.write(class_materials)
+        try:
+            output = generate_lesson_plan_and_materials(grade_level, subject, topic, duration, techniques, complexity, learning_styles)
+            lesson_plan, class_materials = output.split('\n\nClass Materials\n\n')
+            st.header("Generated Lesson Plan")
+            st.write(lesson_plan)
+            st.header("Generated Class Materials")
+            st.write(class_materials)
+        except Exception as e:
+            st.error(f"An error occurred while generating the lesson plan and class materials. Please make sure you have entered a valid API key and try again. Error: {e}")
